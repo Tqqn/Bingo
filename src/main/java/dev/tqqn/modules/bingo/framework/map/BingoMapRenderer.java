645550c9a377d286e1af30b2679c03f41daeee94
Tqqn <mailtoonie@gmail.com>
2025-08-19 23:09:55 +0200

    Some extra work on Perks framework. Like menu

---
+++ src/main/java/dev/tqqn/modules/bingo/framework/map/BingoMapRenderer.java
@@ -0,0 +1,15 @@
+package dev.tqqn.modules.bingo.framework.map;
+
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+import org.jetbrains.annotations.NotNull;
+
+public final class BingoMapRenderer extends MapRenderer {
+
+    @Override
+    public void render(@NotNull MapView mapView, @NotNull MapCanvas mapCanvas, @NotNull Player player) {
+
+    }
+}
