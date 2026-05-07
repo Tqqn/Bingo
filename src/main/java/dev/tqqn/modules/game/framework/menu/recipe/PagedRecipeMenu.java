7d52a960b4f6fe66b00de35583f216ee087d73d0
Tqqn <mailtoonie@gmail.com>
2026-05-07 17:24:54 +0200

    feat(menu): add FurnaceRecipe support and enhance recipe navigation
    - Added support for `FurnaceRecipe` in `PagedRecipeMenu`.

--- src/main/java/dev/tqqn/modules/game/framework/menu/recipe/PagedRecipeMenu.java
+++ src/main/java/dev/tqqn/modules/game/framework/menu/recipe/PagedRecipeMenu.java
@@ -24,11 +24,19 @@ public class PagedRecipeMenu extends PagedMenu {
 
     private final List<RecipeShapePart> shapeParts = new ArrayList<>();
 
-    public PagedRecipeMenu(Player viewer, ItemStack itemStack) {
+    private PagedRecipeMenu possibleOldMenu = null;
+
+    public PagedRecipeMenu(Player viewer, ItemStack itemStack, PagedRecipeMenu possibleOldMenu) {
         super(itemStack.getItemMeta().itemName(), 6, viewer);
         this.itemStack = itemStack;
-        recipes = Bukkit.getServer().getRecipesFor(itemStack).stream().filter(recipe -> recipe instanceof ShapedRecipe || recipe instanceof ShapelessRecipe).toList();
+        this.possibleOldMenu = possibleOldMenu;
+        System.out.println(Bukkit.getServer().getRecipesFor(itemStack));
+        recipes = Bukkit.getServer().getRecipesFor(itemStack).stream().filter(recipe -> recipe instanceof ShapedRecipe || recipe instanceof ShapelessRecipe || recipe instanceof FurnaceRecipe).toList();
         initialize();
+    }
+
+    public PagedRecipeMenu(Player viewer, ItemStack itemStack) {
+        this(viewer, itemStack, null);
     }
 
     @Override
@@ -46,7 +54,11 @@ public class PagedRecipeMenu extends PagedMenu {
         shapeParts.add(new RecipeShapePart(List.of(10, 11, 12)));
         shapeParts.add(new RecipeShapePart(List.of(19, 20, 21)));
         shapeParts.add(new RecipeShapePart(List.of(28, 29, 30)));
-        addPageButton(PageButton.CLOSE, 45);
+        if (possibleOldMenu != null) {
+            registerButton(new MenuButton(ItemBuilder.getBuilder(Material.BARRIER).setDisplayName("<red>Go Back - Previous Recipe").build()), 45);
+        } else {
+            addPageButton(PageButton.CLOSE, 45);
+        }
         if (recipes.size() > 1) {
             addPageButton(PageButton.BACK, 48);
             addPageButton(PageButton.HOME, 49);
@@ -65,6 +77,8 @@ public class PagedRecipeMenu extends PagedMenu {
         if (recipe == null) return;
         if (shapePartsCopy.isEmpty()) return;
 
+        RecipeType recipeType = null;
+
         if (recipe instanceof ShapedRecipe shapedRecipe) {
             final Map<Character, RecipeChoice> ingredientMap = shapedRecipe.getChoiceMap();
             for (String value : shapedRecipe.getShape()) {
@@ -76,10 +90,11 @@ public class PagedRecipeMenu extends PagedMenu {
                     final RecipeChoice recipeChoice = ingredientMap.get(c);
                     if (recipeChoice == null) continue;
                     if (recipeChoice instanceof RecipeChoice.MaterialChoice materialChoice) {
-                        registerButton(getRecipePart(materialChoice.getItemStack()), slot);
+                        registerButton(getRecipeIngredientButton(materialChoice.getItemStack()), slot);
                     }
                 }
             }
+            recipeType = RecipeType.CRAFTING_TABLE;
         }
 
         if (recipe instanceof ShapelessRecipe shapelessRecipe) {
@@ -89,31 +104,71 @@ public class PagedRecipeMenu extends PagedMenu {
                     final RecipeShapePart recipeShapePart = shapePartsCopy.removeFirst();
                     final int slot = recipeShapePart.getNextAvailableSlot();
                     if (slot < 0) continue;
-                    registerButton(getRecipePart(materialChoice.getItemStack()), slot);
+                    registerButton(getRecipeIngredientButton(materialChoice.getItemStack()), slot);
                 }
             }
+            recipeType = RecipeType.CRAFTING_TABLE;
+        }
+
+        if (recipe instanceof FurnaceRecipe furnaceRecipe) {
+            final RecipeChoice recipeChoice = furnaceRecipe.getInputChoice();
+            if (recipeChoice instanceof RecipeChoice.MaterialChoice materialChoice) {
+                registerButton(getRecipeIngredientButton(materialChoice.getItemStack()), 11);
+                registerButton(getRecipeIngredientButton(recipe.getResult()), 25);
+            }
+            recipeType = RecipeType.FURNACE;
         }
 
         registerButton(new MenuButton(recipe.getResult()), 25);
 
-        fillInventoryBorder();
+        fillInventoryBorder(recipeType);
     }
 
-    public void fillInventoryBorder() {
-        final List<Integer> graySlots = List.of(5, 6, 7, 8, 14, 23, 32, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53);
-        final List<Integer> orangeSlots = List.of(0, 4, 36, 40);
-        final List<Integer> brownSlots = List.of(1, 2, 3, 9, 13, 18, 22, 27, 31, 37, 38, 39);
-        final List<Integer> limeGreenSlots = List.of(16, 24, 26, 34);
-        final List<Integer> greenSlots = List.of(15, 17, 33, 35);
+    private MenuButton getRecipeIngredientButton(ItemStack itemStack) {
+        final MenuButton button = new MenuButton(itemStack);
+        final List<Recipe> possibleRecipes = Bukkit.getServer().getRecipesFor(itemStack);
+        if (possibleRecipes.isEmpty()) return button;
 
-        setBorderBasedOfList(graySlots, Material.GRAY_STAINED_GLASS_PANE);
-        setBorderBasedOfList(orangeSlots, Material.ORANGE_STAINED_GLASS_PANE);
-        setBorderBasedOfList(brownSlots, Material.BROWN_STAINED_GLASS_PANE);
-        setBorderBasedOfList(limeGreenSlots, Material.LIME_STAINED_GLASS_PANE);
-        setBorderBasedOfList(greenSlots, Material.GREEN_STAINED_GLASS_PANE);
+        button.setClicker(player -> {
+            final PagedRecipeMenu newMenu = new PagedRecipeMenu(player, itemStack, this);
+            newMenu.open();
+        });
+        return button;
     }
 
-    private void setBorderBasedOfList(List<Integer> slots, Material material) {
+    private void fillInventoryBorder(RecipeType recipeType) {
+        if (recipeType == null) return;
+        switch (recipeType) {
+            case CRAFTING_TABLE -> {
+                final List<Integer> graySlots = List.of(5, 6, 7, 8, 14, 23, 32, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53);
+                final List<Integer> orangeSlots = List.of(0, 4, 36, 40);
+                final List<Integer> brownSlots = List.of(1, 2, 3, 9, 13, 18, 22, 27, 31, 37, 38, 39);
+                final List<Integer> limeGreenSlots = List.of(16, 24, 26, 34);
+                final List<Integer> greenSlots = List.of(15, 17, 33, 35);
+
+                setSlotsBasedOfList(graySlots, Material.GRAY_STAINED_GLASS_PANE);
+                setSlotsBasedOfList(orangeSlots, Material.ORANGE_STAINED_GLASS_PANE);
+                setSlotsBasedOfList(brownSlots, Material.BROWN_STAINED_GLASS_PANE);
+                setSlotsBasedOfList(limeGreenSlots, Material.LIME_STAINED_GLASS_PANE);
+                setSlotsBasedOfList(greenSlots, Material.GREEN_STAINED_GLASS_PANE);
+            }
+
+            case FURNACE -> {
+                final List<Integer> blackSlots = List.of(0, 1, 2, 3, 4, 9, 13, 18, 19, 20, 21, 22, 27, 31, 36, 37, 38, 39, 40);
+                final List<Integer> brownSlots = List.of(10, 12);
+                final List<Integer> whiteSlots = List.of(5, 6, 7, 8, 14, 23, 32, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53);
+                final List<Integer> fireSlots = List.of(28, 29, 30);
+
+                setSlotsBasedOfList(blackSlots, Material.BLACK_STAINED_GLASS_PANE);
+                setSlotsBasedOfList(brownSlots, Material.BROWN_STAINED_GLASS_PANE);
+                setSlotsBasedOfList(whiteSlots, Material.WHITE_STAINED_GLASS_PANE);
+                setSlotsBasedOfList(fireSlots, Material.CAMPFIRE);
+            }
+        }
+
+    }
+
+    private void setSlotsBasedOfList(List<Integer> slots, Material material) {
         for (int slot : slots) {
             if (!isSlotEmpty(slot)) continue;
             registerButton(new MenuButton(ItemBuilder.getBuilder(material).removeDisplayName().build()), slot);
@@ -129,11 +184,6 @@ public class PagedRecipeMenu extends PagedMenu {
     @Override
     public int getPageCount() {
         return recipes.size() - 1;
-    }
-
-    private MenuButton getRecipePart(ItemStack itemStack) {
-        if (itemStack == null) return null;
-        return new MenuButton(itemStack).setClicker(player -> close());
     }
 
     public class RecipeShapePart implements Cloneable {
@@ -153,5 +203,10 @@ public class PagedRecipeMenu extends PagedMenu {
         protected RecipeShapePart clone() {
             return new RecipeShapePart(List.copyOf(availableSlots));
         }
+    }
+
+    private enum RecipeType {
+        CRAFTING_TABLE,
+        FURNACE
     }
 }
